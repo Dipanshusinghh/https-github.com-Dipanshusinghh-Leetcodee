24e44afe4c452f14f5515bacb1302d16649e1664
Dipanshu singh <161134993+Dipanshusinghh@users.noreply.github.com>
2026-07-30 00:36:51 +0530

    Time: 44 ms (92.31%), Space: 54.7 MB (92.77%) - LeetHub

---
+++ 3499-maximize-active-section-with-trade-i/3499-maximize-active-section-with-trade-i.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxActiveSectionsAfterTrade(string s) {
+        int n = s.size();
+        int ones = 0;
+        int best = 0;
+        int prevZero = INT_MIN;
+        for (int i = 0; i < n; ) {
+            int j = i + 1;
+            while (j < n && s[j] == s[i])
+                ++j;
+            int len = j - i;
+            if (s[i] == '1') {
+                // Count existing active sections.
+                ones += len;
+            } else {
+                // Merge two neighboring zero blocks.
+                best = max(best, prevZero + len);
+                prevZero = len;
+            }
+            i = j;
+        }
+        return ones + best;
+    }
+};
